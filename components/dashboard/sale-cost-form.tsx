5ea281e8fcc42f59b1dec813a10e8eb54f5aa5c3
João Vitor Schwingel <jv.schwingel@gmail.com>
2026-06-03 18:15:20 -0300

    add estoque as cost type

--- components/dashboard/sale-cost-form.tsx
+++ components/dashboard/sale-cost-form.tsx
@@ -17,7 +17,7 @@ interface SaleCostFormProps {
   onCancel: () => void
 }
 
-const COST_TYPES = ["Compra","Transporte", "Tarifas", "Comissão Extra", "Embalagem", "Impostos", "Frete", "Seguro", "Outros"]
+const COST_TYPES = ["Compra","Transporte", "Tarifas", "Comissão Extra", "Embalagem", "Estoque", "Impostos", "Frete", "Seguro", "Outros"]
 
 export function SaleCostForm({ saleId, onSuccess, onCancel }: SaleCostFormProps) {
   const [isSubmitting, setIsSubmitting] = useState(false)
